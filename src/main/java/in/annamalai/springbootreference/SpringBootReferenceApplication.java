ed32c1468978da5b0b3b2c91f3febd36a1ee1e36
Annamalai Palanikumar <annamalai@outlook.in>
2022-02-14 18:13:04 +0100

    Initial Commit

---
+++ src/main/java/in/annamalai/springbootreference/SpringBootReferenceApplication.java
@@ -0,0 +1,13 @@
+package in.annamalai.springbootreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReferenceApplication.class, args);
+	}
+
+}
