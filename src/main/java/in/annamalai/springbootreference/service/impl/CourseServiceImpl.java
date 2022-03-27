fe90a0ba5269cb27e9d81b0e2eb95e13204a3e5d
Annamalai Palanikumar <annamalai@outlook.in>
2022-03-27 23:32:10 +0200

    bootup problem fix

--- src/main/java/in/annamalai/springbootreference/service/impl/CourseServiceImpl.java
+++ src/main/java/in/annamalai/springbootreference/service/impl/CourseServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import in.annamalai.springbootreference.model.Course;
 import in.annamalai.springbootreference.repository.CourseRepository;
 import in.annamalai.springbootreference.service.CourseService;
 
+@Service
 public class CourseServiceImpl implements CourseService {
 
     @Autowired
